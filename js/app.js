13414c69216b0872d478a65edba42dc4b09d5d39
Chris Klanac <cklanac@users.noreply.github.com>
2017-12-05 09:41:23 -0600

    correct app.js

--- js/app.js
+++ js/app.js
@@ -1,37 +1,187 @@
-/* global jQuery, Api, Renderer */
+/* global jQuery, handle, $, api */
 'use strict';
 
-const ITEMS_URL = '/v1/notes/';
+const ITEMS_URL = '/api/v1/stories/';
 
-class Store {
-  constructor() {
-    this.view = 'list'; // current view: list | details | create | edit 
-    this.query = {};    // search query values
-    this.list = null;   // search result - array of objects (documents)
-    this.item = null;   // currently selected document
+const renderPage = function (store) {
+  if (store.demo) {
+    $('.view').css('background-color', 'gray');
+    $('#' + store.view).css('background-color', 'white');
+  } else {
+    $('.view').hide();
+    $('#' + store.view).show();
   }
-}
+};
 
-const api = new Api();
-const store = new Store();
-const render = new Renderer(store, api);
+const renderResults = function (store) {
+  const listItems = store.list.map((item) => {
+    return `<li id="${item.id}">
+                <a href="${item.url}" class="detail">${item.title}</a>
+              </li>`;
+  });
+  $('#result').empty().append('<ul>').find('ul').append(listItems);
+};
+
+const renderEdit = function (store) {
+  const el = $('#edit');
+  const item = store.item;
+  el.find('[name=title]').val(item.title);
+  el.find('[name=content]').val(item.content);
+}; 
+
+const renderDetail = function (store) {
+  const el = $('#detail');
+  const item = store.item;
+  el.find('.title').text(item.title);
+  el.find('.content').text(item.content);
+};
+
+const handleSearch = function (event) {
+  event.preventDefault();
+  const store = event.data;
+  const el = $(event.target);
+  const title = el.find('[name=title]').val();
+  var query;
+  if (title) {
+    query = {
+      title: el.find('[name=title]').val()
+    };
+  }
+  api.search(query)
+    .then(response => {
+      store.list = response;
+      renderResults(store);
+
+      store.view = 'search';
+      renderPage(store);
+    }).catch(err => {
+      console.error(err);
+    });
+};
+
+const handleCreate = function (event) {
+  event.preventDefault();
+  const store = event.data;
+  const el = $(event.target);
+
+  const document = {
+    title: el.find('[name=title]').val(),
+    content: el.find('[name=content]').val()
+  };
+  api.create(document)
+    .then(response => {
+      store.item = response;
+      store.list = null; //invalidate cached list results
+      renderDetail(store);
+      store.view = 'detail';
+      renderPage(store);
+    }).catch(err => {
+      console.error(err);
+    });
+};
+
+const handleUpdate = function (event) {
+  event.preventDefault();
+  const store = event.data;
+  const el = $(event.target);
+
+  const document = {
+    id: store.item.id,
+    title: el.find('[name=title]').val(),
+    content: el.find('[name=content]').val()
+  };
+  api.update(document, store.token)
+    .then(response => {
+      store.item = response;
+      store.list = null; //invalidate cached list results
+      renderDetail(store);
+      store.view = 'detail';
+      renderPage(store);
+    }).catch(err => {
+      console.error(err);
+    });
+};
+
+const handleDetails = function (event) {
+  event.preventDefault();
+  const store = event.data;
+  const el = $(event.target);
+
+  const id = el.closest('li').attr('id');
+  api.details(id)
+    .then(response => {
+      store.item = response;
+      renderDetail(store);
+
+      store.view = 'detail';
+      renderPage(store);
+
+    }).catch(err => {
+      store.error = err;
+    });
+};
+
+const handleRemove = function (event) {
+  event.preventDefault();
+  const store = event.data;
+  const id = store.item.id;
+
+  api.remove(id, store.token)
+    .then(() => {
+      store.list = null; //invalidate cached list results
+      return handleSearch(event);
+    }).catch(err => {
+      console.error(err);
+    });
+};
+const handleViewCreate = function (event) {
+  event.preventDefault();
+  const store = event.data;
+  store.view = 'create';
+  renderPage(store);
+};
+const handleViewList = function (event) {
+  event.preventDefault();
+  const store = event.data;
+  if (!store.list) {
+    handleSearch(event);
+    return;
+  }
+  store.view = 'search';
+  renderPage(store);
+};
+const handleViewEdit = function (event) {
+  event.preventDefault();
+  const store = event.data;
+  renderEdit(store);
+
+  store.view = 'edit';
+  renderPage(store);
+};
 
 //on document ready bind events
 jQuery(function ($) {
 
-  $('#search').on('submit', (e) => render.search(e));
-  $('#edit').on('submit', (e) => render.update(e));
-  $('#create').on('submit', render.create);
+  const STORE = {
+    demo: false,        // display in demo mode true | false
+    view: 'list',       // current view: list | details | create | edit 
+    query: {},          // search query values
+    list: null,         // search result - array of objects (documents)
+    item: null,         // currently selected document
+  };
 
-  $('#result').on('click', '.detail', (e) => render.details(e));
-  $('#detail').on('click', '.remove', render.remove);
-  $('#detail').on('click', '.edit', (e) => render.showEdit(e));
+  $('#create').on('submit', STORE, handleCreate);
+  $('#search').on('submit', STORE, handleSearch);
+  $('#edit').on('submit', STORE, handleUpdate);
 
-  $(document).on('click', '.viewCreate', render.showCreate);
-  $(document).on('click', '.viewList', (e) => render.showList(e));
+  $('#result').on('click', '.detail', STORE, handleDetails);
+  $('#detail').on('click', '.remove', STORE, handleRemove);
+  $('#detail').on('click', '.edit', STORE, handleViewEdit);
+
+  $(document).on('click', '.viewCreate', STORE, handleViewCreate);
+  $(document).on('click', '.viewList', STORE, handleViewList);
 
   // start app by triggering a search
   $('#search').trigger('submit');
 
 });
-
